7cd3cddf189e6e5dfb42ead4ff4fe728c8672543
Kole565 <rjkz565rjkz565@gmail.com>
2021-10-19 18:25:02 +0300

    Add full_title, signup page tests

---
+++ test/helpers/application_helper_test.rb
@@ -0,0 +1,13 @@
+require "test_helper"
+
+class ApplicationHelperTest < ActionDispatch::IntegrationTest
+	# test "the truth" do
+	#	 assert true
+	# end
+
+	test "full_title" do
+		assert_equal full_title(), "Ruby on Rails Tutorial Sample App"
+		assert_equal full_title("test name"), "test name | Ruby on Rails Tutorial Sample App"
+	end
+	
+end
