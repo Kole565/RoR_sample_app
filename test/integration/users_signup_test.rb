789490058a45ac39cce7941d42ac62559953a884
Kole565 <rjkz565rjkz565@gmail.com>
2021-10-28 21:41:11 +0300

    Activate ssl using option. Fix user signup tests

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -6,7 +6,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
 		get signup_path
 
 		assert_no_difference "User.count" do
-			User.new(
+			user = User.new(
 				{
 					name: "",
 					email: "invalid@mail",
@@ -14,6 +14,25 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
 					password_confirmation: "bar" 
 				}
 			)
+			user.save
+		end
+
+		assert_template "users/new"
+	end
+
+	test "should register valid user" do
+		get signup_path
+
+		assert_difference "User.count" do
+			user = User.new(
+				{
+					name: "Joe",
+					email: "valid@gmail.com",
+					password: "password",
+					password_confirmation: "password" 
+				}
+			)
+			user.save
 		end
 
 		assert_template "users/new"
